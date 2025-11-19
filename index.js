038a24fa5a75e4fff8782502912386c4877c026e
tylekmj-ops <tylekmj@gmail.com>
2025-11-19 15:45:36 -0500

    completed

--- index.js
+++ index.js
@@ -1,33 +1,64 @@
 /**
  * Creature Class
- * 
+ *
  * A superclass for magical creatures with name and habitat.
  */
 
 class Creature {
   // write your code here
+  constructor(name, habitat) {
+    this.name = name;
+    this.habitat = habitat;
+  }
+
+  describe() {
+    return `${this.name} lives in the ${this.habitat}.`;
+  }
+
+  sleep() {
+    return `${this.name} falls asleep in the ${this.habitat}. 😴`;
+  }
 }
 
 /**
  * Dragon Class
- * 
+ *
  * A subclass of Creature that can breathe fire.
  */
 
 class Dragon extends Creature {
- //write your code here
+  //write your code here
+  constructor(name, habitat, firePower) {
+    super(name, habitat);
+    this.firePower = firePower;
+  }
+
+  specialMove(){
+    return `${this.name} breathes fire with ${this.firePower} intensity! 🔥`
+  }
 }
 
 /**
  * Unicorn Class
- * 
+ *
  * A subclass of Creature that can heal with sparkles.
  */
 
 class Unicorn extends Creature {
   //write your code here
+  constructor(name, habitat, sparkleLevel) {
+    super(name, habitat);
+    this.sparkleLevel = sparkleLevel
+  }
+
+  specialMove() {
+    return `${this.name} heals allies with a sparkle level of ${this.sparkleLevel}! ✨`
+  }
 }
+
+const dragon = new Creature("frire", "snow")
+console.log(dragon.describe(), dragon.sleep())
+
 
 // Export the classes for testing
 module.exports = { Creature, Dragon, Unicorn };
-
